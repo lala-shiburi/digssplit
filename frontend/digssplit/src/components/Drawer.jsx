5984745cbe5212556da4947e6ff3a24c25535acb
lala <nhlalala@nshiburi.co.za>
2019-07-15 21:27:32 +0200

    Adds Icons and text to the drawer

--- frontend/digssplit/src/components/Drawer.jsx
+++ frontend/digssplit/src/components/Drawer.jsx
@@ -6,59 +6,60 @@ import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import AddIcon from '@material-ui/icons/Add';
+import { Add, PersonAdd, RemoveRedEye } from '@material-ui/icons';
 import MailIcon from '@material-ui/icons/Mail';
 
+import CustomButton from './Button'
+
 const useStyles = makeStyles({
-  list: {
-    width: 250,
-  },
-  fullList: {
-    width: 'auto',
-  },
+	list: {
+		width: 250
+	},
+	fullList: {
+		width: 'auto'
+	}
 });
 
 export default function TemporaryDrawer(props) {
-  const classes = useStyles();
-  
- const{toggleDrawer,drawer}=props
+	const classes = useStyles();
 
+	const { toggleDrawer, drawer } = props;
 
-  const sideList = side => (
-    <div
-      className={classes.list}
-      role="presentation"
-      onClick={toggleDrawer(false)}
-      onKeyDown={toggleDrawer(false)}
-    >
-      <List>
-        {['ADD AN EXPENSE','ADD A DIGSMATE','VIEW EXPENSES'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>{index === 0 ? <AddIcon /> : <MailIcon />}</ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-    </div>
-  );
+	const sideList = side => (
+		<div
+			className={classes.list}
+			role="presentation"
+			onClick={toggleDrawer(false)}
+			onKeyDown={toggleDrawer(false)}
+		>
+			<List>
+				{['ADD AN EXPENSE', 'ADD A DIGSMATE', 'VIEW EXPENSES'].map(
+					(text, index) => (
+						<ListItem button key={text}>
+							<ListItemIcon>
+								{index === 0 ? (
+									<Add />
+								) : index === 1 ? (
+									<PersonAdd />
+								) : (
+									<RemoveRedEye />
+								)}
+							</ListItemIcon>
+							<ListItemText primary={text} />
+						</ListItem>
+					)
+				)}
+			</List>
+			<Divider />
+			<CustomButton text={'LOG IN/SIGN UP'}  />
+		</div>
+	);
 
-
-
-  return (
-    <div>
-      
-      <Drawer anchor="right" open={drawer} onClose={toggleDrawer(false)}>
-        {sideList('right')}
-      </Drawer>
-    </div>
-  );
+	return (
+		<div>
+			<Drawer anchor="right" open={drawer} onClose={toggleDrawer(false)}>
+				{sideList('right')}
+			</Drawer>
+		</div>
+	);
 }
