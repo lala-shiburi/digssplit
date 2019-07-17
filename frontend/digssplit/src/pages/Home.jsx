3e938ae07f1827b1e10f75333db646bef06d6dbe
lala <nhlalala@nshiburi.co.za>
2019-07-17 18:42:34 +0200

    Adds some touches to the home page

--- frontend/digssplit/src/pages/Home.jsx
+++ frontend/digssplit/src/pages/Home.jsx
@@ -1,36 +1,52 @@
-import React, { Component,} from 'react';
-import Typography from '@material-ui/core/Typography';
+import React, { Component } from 'react';
+import { Typography, Grid, Paper } from '@material-ui/core';
 
 import CustomButton from '../components/Button';
 
-
-
 export default class Home extends Component {
+	handlesClick = ButtonEvent => {
+		console.log('You clicked the signin button');
+	};
 
-   
-    
-    handlesClick=ButtonEvent=>{
-        console.log("You clicked the signin button");
-    }
+	render() {
+		const styles = {
+            mainContainer:{
+              position:'relative',
+              top:'17vh'  
+            },
+			heading: {
+				color: 'White',
+                textShadow: '2px 1px 2px rgba(0, 0, 0, 0.98)',
+                fontSize:'45px',
+            },
+            paper:{
+                textAlign:'center',
+                backgroundColor: 'rgba(255,255,255,0.6)'
+            },
+            button:{
+                fontSize:'20px',
+            }
+		};
 
-    
-    render() {
+		return (
+			<React.Fragment>
+				<Grid container style={styles.mainContainer}>
+					<Grid item xs={12}>
+						<Typography variant="h4" gutterBottom style={styles.heading}>
+							SPLIT BILLS WITH YOUR DIGSMATES
+						</Typography>
+					</Grid>
 
-        const styles={
-            
-        }
-        
-        return (
-            <React.Fragment>
-                
-                    <Typography variant="h4" gutterBottom style={{color:"White",textShadow: "2px 1px 2px rgba(0, 0, 0, 0.98)"}}>
-                        SPLIT BILLS WITH YOUR DIGSMATES
-                    </Typography>
-
-                    <CustomButton text={"LOG IN/SIGN UP"} onClick={this.handlesClick} ></CustomButton>
-
-
-            </React.Fragment>
-        )
-    }
+					<Grid item xs={12}>
+						<Paper elevation="14" style={styles.paper}>
+							<CustomButton style={styles.button}
+								text={'LOG IN / SIGN UP'}
+								onClick={this.handlesClick}
+							/>
+						</Paper>
+					</Grid>
+				</Grid>
+			</React.Fragment>
+		);
+	}
 }
